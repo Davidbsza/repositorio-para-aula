fc65f926ef2d4014e2bf8004d2163b72917f7462
Davidbsza <davidbbsouza@gmail.com>
2025-10-20 19:44:45 -0300

    atualizado

--- desafio_5_notas.py
+++ desafio_5_notas.py
@@ -1,14 +1,18 @@
 # Programa: soma de duas notas
-
+nome = input("digite seu nome: ")
 # Captura das notas
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("digite a terceira nota: "))
+nota4 = float(input("digite a quarta nota "))
+
 
 # Soma das notas
-soma = nota1 + nota2
+
+soma = (nota1 + nota2 + nota3 + nota4 ) /4
+
 
 # Exibição do resultado
+
 print("\n--- Resultado ---")
-print(f"Nota 1: {nota1}")
-print(f"Nota 2: {nota2}")
-print(f"Soma das notas: {soma:.2f}")
+print(f"Soma das notas: {nome} sua media é {soma:.2f}")
