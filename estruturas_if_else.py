21ce56cd1cec0e02faf3b81c74514fbd54924f5e
Davidbsza <davidbbsouza@gmail.com>
2025-10-21 08:04:46 -0300

    estruturas basica

---
+++ estruturas_if_else.py
@@ -0,0 +1,6 @@
+# Solicita a idade do usuário
+idade = int(input("Digite sua idade: "))
+
+# Verifica se é maior ou igual a 18
+if idade >= 18:
+    print("Indivíduo possui idade mínima para dirigir")
